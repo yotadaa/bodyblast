0ea0eb061cb9ea978b33e655910316a81f30fec2
yotadaa <mukhtadanasution@gmail.com>
2024-06-16 23:50:03 +0700

    fixed testimonials

---
+++ database/migrations/2024_06_16_164903_reset_testimonials.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('table_name');
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->string("name");
+            $table->string("message");
+            $table->string("role");
+            $table->string("foto");
+            $table->id()->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
